5d3bbff048849cef315c065daea823e7c9a18147
Kamiko <83876411+Cod-kd@users.noreply.github.com>
2023-02-06 14:58:41 +0100

    login

--- log-reg/app.js
+++ log-reg/app.js
@@ -1,12 +1,12 @@
+//npm i express mysql dotenv //hbs bcryptjs//
 /* indítás: "npm start" (package.json) / "node app.js" */
 /* !FONTOS: adatbázis elindítása pl.: xampp-ról a .env-s konfigurációkkal */
 // használt: express, mysql, dotenv, hbs, bcryptjs
 const express = require('express'); // applikáció kezeléséhez
 const mysql = require("mysql"); // kapcsolatfelvételre az adatbázissal
 const dotenv = require('dotenv'); // .env-hez nem publikus konfigok elrejtésére
-const bcrypt = require("bcryptjs"); // titkosítás jelszóhoz .hash / .compare => 0/1
+//const bcrypt = require("bcryptjs"); // titkosítás jelszóhoz .hash / .compare => 0/1
 const app = express();
-
 // lekérés .env fájlból & kapcsolat az DB-vel
 dotenv.config({ path: './.env' });
 const db = mysql.createConnection({
@@ -29,9 +29,9 @@ db.connect((error) => {
 
 app.set('view engine', 'hbs'); // hbs-t választottam igazából nem számít
 
-const path = require("path");
-const publicDir = path.join(__dirname, './public');
-app.use(express.static(publicDir));
+const path = require("path"); // ha más könyvtárból lenne futtatva
+const { table } = require('console');
+app.use(express.static(path.join(__dirname, './public'))); // "publikus" mappa használata 
 
 // index href lekérése
 app.get("/", (req, res) => {
@@ -53,11 +53,9 @@ app.get("/login", (req, res) => {
     res.render("login")
 });
 
+/* POST-hoz, req.body-hez */
 app.use(express.urlencoded({ extended: 'false' }));
 app.use(express.json());
-
-logV() // kiír minden adatot az adatbázisból
-
 // register form action-je
 app.post("/auth/register", async (req, res) => {
 
@@ -70,7 +68,7 @@ app.post("/auth/register", async (req, res) => {
     } 
     else {
         // helyes paraméterek esetén insert
-        db.query('INSERT INTO users SET ?', { name: name, email: email, password: await bcrypt.hash(password, 8) }, (err, result) => {
+        db.query('INSERT INTO users SET ?', { name: name, email: email, password: password /*await bcrypt.hash(password, 8)*/ }, (err, result) => {
             if (err) {
                 console.log(err)
             } else {
@@ -82,9 +80,38 @@ app.post("/auth/register", async (req, res) => {
         }
 });
 
-function logV(){
-    db.query("SELECT * FROM users", (err, result, fields) => {
+app.post("/auth/login", (req, res) => {
+    const { name, password } = req.body;
+
+    db.query(`SELECT * FROM users WHERE name = "${name}" AND password = "${password}"`, (err, result)=>{
+        if(err){
+            console.log(err)
+        } else {
+            return res.render('login', {
+                data: JSON.stringify(result) //stringként
+            });
+        }
+    })
+});
+
+function logUser(user){
+    db.query(`SELECT * FROM users WHERE name = "${user}"`, (err, result, fields) => {
         if (err) throw err;
-        console.log(result);
+        console.log(copy(result)[0])
     });
+}
+
+function logUsers(){
+    db.query(`SELECT * FROM users`, (err, result, fields) => {
+        if (err) throw err;
+        console.log(copy(result)[0])
+    });
+}
+
+function copy(data) {
+	return JSON.parse(JSON.stringify(data));
+}
+
+function deleteRec(table, where){
+    db.query(`DELETE FROM ${table} WHERE ${where}`);
 }
